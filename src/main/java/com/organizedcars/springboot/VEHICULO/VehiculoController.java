52ee794625534c3f8887a60e6321c6099e19b9fe
Fernando Abelini <ferabelini@gmail.com>
2020-10-25 10:19:02 -0300

    Allow all origins

--- src/main/java/com/organizedcars/springboot/VEHICULO/VehiculoController.java
+++ src/main/java/com/organizedcars/springboot/VEHICULO/VehiculoController.java
@@ -3,6 +3,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Vehiculos")
+@CrossOrigin(origins = "*")
 public class VehiculoController {	
 	
     @Autowired
