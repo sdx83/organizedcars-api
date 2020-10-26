c5e47f3e39d6ec4474c2744c8b136cd3dc1e9f15
Sergio <sgonzalez.sergiod@gmail.com>
2020-10-25 22:47:34 -0300

    Se ordenan los servicios alfabeticamente....

--- src/main/java/com/organizedcars/springboot/SERVICIO/ServicioServiceImpl.java
+++ src/main/java/com/organizedcars/springboot/SERVICIO/ServicioServiceImpl.java
@@ -1,5 +1,7 @@
 package com.organizedcars.springboot.SERVICIO;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,16 @@ public class ServicioServiceImpl implements ServicioService {
 	
 	@Transactional(readOnly = true)
 	public List<Servicio> findAll() {
-		return servicioDAO.findAll();
+		
+		List<Servicio> servicios = servicioDAO.findAll();
+		
+		//ordeno los mantenimientos por dominio y fecha
+ 		Comparator<Servicio> comparadorServicios = (Servicio s1, Servicio s2) -> {
+ 			return (s1.getNombre()).compareTo((s2.getNombre()));
+ 		};
+ 		
+ 		Collections.sort(servicios, comparadorServicios);
+		 
+		return servicios ;
 	}
 }
