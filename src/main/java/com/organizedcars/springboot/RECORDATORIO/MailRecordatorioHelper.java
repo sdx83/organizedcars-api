8f22c27335f7f73646dced2489e24405062bf9c7
Sergio <gonzalez.sergiod@gmail.com>
2020-11-14 16:14:13 -0300

    prueba enviroment...

--- src/main/java/com/organizedcars/springboot/RECORDATORIO/MailRecordatorioHelper.java
+++ src/main/java/com/organizedcars/springboot/RECORDATORIO/MailRecordatorioHelper.java
@@ -2,11 +2,18 @@ package com.organizedcars.springboot.RECORDATORIO;
 
 import java.util.List;
 
+import org.springframework.core.env.Environment;
+
 import com.organizedcars.springboot.VEHICULORECORDATORIOS.VehiculoRecordatorio;
 
 public class MailRecordatorioHelper {
 
 	public static void enviarNotificaciones(List<VehiculoRecordatorio> recordatorios, String mail) throws Exception {
+		
+		//Prueba cloudinary
+		Environment environment = null;
+		String key = environment.getProperty("api_secret_cloudinary").toString();
+		
 		
 		for (VehiculoRecordatorio vehiculoRecordatorio : recordatorios) {
 			//TODO: rutina para enviar los mails correspondientes por cada recordatorio			
