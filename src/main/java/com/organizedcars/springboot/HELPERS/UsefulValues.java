a6cea628d43f28fc6a56a3890b70f078e9d18812
Sergio <gonzalez.sergiod@gmail.com>
2020-11-18 16:25:39 -0300

    Se toma user y pass del mail de las variables de entorno de heroku...

--- src/main/java/com/organizedcars/springboot/HELPERS/UsefulValues.java
+++ src/main/java/com/organizedcars/springboot/HELPERS/UsefulValues.java
@@ -5,11 +5,9 @@ import java.util.Map;
 
 public final class UsefulValues {
     public static final  Map<String,String > PROPERTIES = new HashMap<>();
-
+    
     static{
         PROPERTIES.put("api_secret_cloudinary","YMATgrzrFGfPtKihy_HY0Dxa4ms");
-        PROPERTIES.put("username","organizedcardsenterprise@gmail.com");
-        PROPERTIES.put("password","OrgCars20202C");
         PROPERTIES.put("dias_aviso_recordatorio","5");
     }
 }
--- src/main/java/com/organizedcars/springboot/RECORDATORIO/MAIL_DEVELOPMENT/JavaMailSenderProperties.java
+++ src/main/java/com/organizedcars/springboot/RECORDATORIO/MAIL_DEVELOPMENT/JavaMailSenderProperties.java
@@ -1,26 +1,30 @@
 package com.organizedcars.springboot.RECORDATORIO.MAIL_DEVELOPMENT;
 
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import com.organizedcars.springboot.HELPERS.UsefulValues;
-
-import java.util.Properties;
-
 @Configuration
 public class JavaMailSenderProperties {
 
+	 @Value("${spring.mail.username}")
+	 private String userMail;
+	 
+	 @Value("${spring.mail.password}")
+	 private String passwordMail;
+	 
     @Bean
     public JavaMailSender getJavaMailSender(){
         JavaMailSenderImpl mailSender=new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername(UsefulValues.PROPERTIES.get("username"));
-        mailSender.setPassword(UsefulValues.PROPERTIES.get("password"));
-
+        mailSender.setUsername(userMail);
+        mailSender.setPassword(passwordMail);
 
         Properties properties=mailSender.getJavaMailProperties();
         properties.put("mail.transport.protocol","smtp");
